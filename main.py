a463ad69e76cc2d1a090ac320faffad840dd58fd
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-30 00:33:05 -0500

    Increase filters and kernel size

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ R_LOSS_FACTOR = 1000
 INPUT_DIM     = (IMG_WIDTH, IMG_HEIGHT, OUTPUT_CHANNELS)
 # Dimensión del espacio latente
 LATENT_DIM    = 150
-EPOCHS        = 50
+EPOCHS        = 100
 INITIAL_EPOCH = 0
 
 use_batch_norm  = True
@@ -283,13 +283,13 @@ class VAE(keras.Model):
         self.latent_dim = LATENT_DIM
         # Utilizamos un número mayor de capas convolucionales para obtener mejor
         # las características del gradiente de entrada
-        self.encoder_conv_filters = [64, 64, 64, 64, 64, 64]
-        self.encoder_conv_kernel_size = [3, 3, 3, 3, 3, 3]
+        self.encoder_conv_filters = [128, 128, 128, 128, 128, 128]
+        self.encoder_conv_kernel_size = [5, 5, 5, 5, 5, 5]
         self.encoder_conv_strides = [2, 2, 2, 2, 2, 2]
         self.n_layers_encoder = len(self.encoder_conv_filters)
 
-        self.decoder_conv_t_filters = [64, 64, 64, 64, 64, OUTPUT_CHANNELS]
-        self.decoder_conv_t_kernel_size = [3, 3, 3, 3, 3, 3]
+        self.decoder_conv_t_filters = [128, 128, 128, 128, 128, OUTPUT_CHANNELS]
+        self.decoder_conv_t_kernel_size = [5, 5, 5, 5, 5, 5]
         self.decoder_conv_t_strides = [2, 2, 2, 2, 2, 2]
         self.n_layers_decoder = len(self.decoder_conv_t_filters)
 
